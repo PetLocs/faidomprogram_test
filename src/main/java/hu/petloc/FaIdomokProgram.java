aca4e46b3dc41cfc4574b90954acfd1089430341
Yclaze <teralocsi@gmail.com>
2025-01-04 17:28:57 +0100

    ProgramTest, három note

--- src/main/java/hu/petloc/FaIdomokProgram.java
+++ src/main/java/hu/petloc/FaIdomokProgram.java
@@ -1,4 +1,67 @@
 package hu.petloc;
 
+import hu.petloc.idomok.FaIdom;
+import hu.petloc.idomok.Gomb;
+import hu.petloc.idomok.Hasab;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class FaIdomokProgram {
+    private ArrayList<FaIdom> idomok;
+
+    public FaIdomokProgram() {
+        this.idomok = new ArrayList<>();
+        idomok.clear();
+    }
+
+    public void ujIdom(FaIdom idom){
+        idomok.add(idom);
+    };
+
+    public ArrayList<FaIdom> getLista() {
+        return this.idomok;
+    }
+
+    public double osszSuly() {
+        double sum = 0;
+        for (FaIdom idom : idomok){
+            sum += idom.suly();
+        }
+        return sum;
+    }
+
+    public double gombokOsszSuly() {
+        double sum = 0;
+        for (FaIdom idom : idomok){
+            if (idom instanceof Gomb) {
+                sum += idom.suly();
+            }
+        }
+        return sum;
+    }
+
+    public FaIdom minV() {
+        double min = Double.POSITIVE_INFINITY;
+        int index = 0;
+        for (int i = 0; i < idomok.size(); i++) {
+            if (idomok.get(i).terfogat() < min){
+                min = idomok.get(i).terfogat();
+                index = i;
+            }
+        }
+        return idomok.get(index);
+    }
+
+    public FaIdom maxV() {
+        double max = 0;
+        int index = 0;
+        for (int i = 0; i < idomok.size(); i++) {
+            if (idomok.get(i).terfogat() > max){
+                max = idomok.get(i).terfogat();
+                index = i;
+            }
+        }
+        return idomok.get(index);
+    }
 }
--- src/test/java/hu/petloc/ProgramTest.java
+++ src/test/java/hu/petloc/ProgramTest.java
@@ -1,43 +1,61 @@
 package hu.petloc;
 
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class ProgramTest {
-//    FaIdomokProgram program;
-//    @BeforeAll
-//    static void programIni(){
-//        Class<?> clazz = FaIdomokProgram.class;
-//        String pkg = clazz.getPackageName();
-//        assertEquals("hu.soter", pkg, "a csomag 'hu.soter' legyen.");
-//
-//        program = new FaIdomokProgram();
-//        program.ujIdom(new Gomb(1));
-//        program.ujIdom(new Hasab(1,1,1));
-//    }
-//
-//    @Test
-//    void testLista(){
-//        ArrayList<FaIdom> lista = program.getLista();
-//        Assertions.assertEquals(2, lista.size());
-//
-//        lista.add(new Gomb(1));
-//        Assertions.assertEquals(2, program.getLista().size());
-//    }
-//
-//    @Test
-//    void testSzamolasok(){
-//        Assertions.assertEquals(4.16, program.osszSuly(), 0.001);
-//        Assertions.assertEquals(3.36, program.gombokOsszSuly(), 0.001);
-//
-//        Hasab hasab = new Hasab(1.0, 1.0, 1.0);
-//        Assertions.assertEquals(hasab.terfogat(), program.minV().terfogat());
-//        Gomb gomb = new Gomb(1.0);
-//        Assertions.assertEquals(gomb.terfogat(), program.maxV().terfogat());
-//    }
-//}
+import hu.petloc.idomok.FaIdom;
+import hu.petloc.idomok.Gomb;
+import hu.petloc.idomok.Hasab;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProgramTest {
+    //FaIdomokProgram program;
+    /* vagy static a program vagy nem static az Ini */
+    static FaIdomokProgram program;
+
+    @BeforeAll
+    static void programIni(){
+        Class<?> clazz = FaIdomokProgram.class;
+        String pkg = clazz.getPackageName();
+        //assertEquals("hu.soter", pkg, "a csomag 'hu.soter' legyen.");
+        /*egyedi package mindenkinél */
+        assertEquals("hu.petloc", pkg, "a csomag 'hu.petloc' legyen.");
+
+        program = new FaIdomokProgram();
+        program.ujIdom(new Gomb(1));
+        program.ujIdom(new Hasab(1,1,1));
+    }
+
+    @Test
+    void testLista(){
+        ArrayList<FaIdom> lista = program.getLista();
+        Assertions.assertEquals(2, lista.size());
+
+        lista.add(new Gomb(1));
+        //Assertions.assertEquals(2, program.getLista().size());
+        /* méret tévesen lett megadva */
+        Assertions.assertEquals(3, program.getLista().size());
+    }
+
+    @Test
+    void testSzamolasok(){
+        //Assertions.assertEquals(4.16, program.osszSuly(), 0.001);
+        //Assertions.assertEquals(3.36, program.gombokOsszSuly(), 0.001);
+        /* hiányzik az első gömb súlya */
+        Assertions.assertEquals(7.502, program.osszSuly(), 0.001);
+        Assertions.assertEquals(6.702, program.gombokOsszSuly(), 0.001);
+
+        //Hasab hasab = new Hasab(1.0, 1.0, 1.0);
+        /* az új idom nem része a listának, ujIdom() metódus szükséges */
+        Hasab hasab = new Hasab(1.0, 1.0, 1.0);
+        program.ujIdom(hasab);
+        Assertions.assertEquals(hasab.terfogat(), program.minV().terfogat());
+        Gomb gomb = new Gomb(1.0);
+        /* az új idom nem része a listának, ujIdom() metódus szükséges */
+        program.ujIdom(gomb);
+        Assertions.assertEquals(gomb.terfogat(), program.maxV().terfogat());
+    }
+}
